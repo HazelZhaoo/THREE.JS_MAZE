cbc96bc0f0dc3c383f38528a5fa02d2da35ee925
OatLatte11 <109826753+HazelZhaoo@users.noreply.github.com>
2023-05-04 18:56:03 -0700

    Create stack.js

---
+++ stack.js
@@ -0,0 +1,26 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    if (this.items.length == 0) {
+      return "Underflow";
+    }
+    return this.items.pop();
+  }
+
+  top() {
+    return this.items[this.items.length - 1];
+  }
+
+  empty() {
+    return this.items.length == 0;
+  }
+}
+
+export { Stack };
