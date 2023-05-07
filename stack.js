7025fb9688e408e7513f79e50967c6f4d9655fc7
OatLatte11 <109826753+HazelZhaoo@users.noreply.github.com>
2023-05-06 21:24:08 -0700

    Update stack.js

--- stack.js
+++ stack.js
@@ -1,26 +1,26 @@
 class Stack {
-  constructor() {
-    this.items = [];
-  }
-
-  push(element) {
-    this.items.push(element);
-  }
-
-  pop() {
-    if (this.items.length == 0) {
-      return "Underflow";
+    constructor() {
+      this.items = [];
     }
-    return this.items.pop();
+  
+    push(element) {
+      this.items.push(element);
+    }
+  
+    pop() {
+      if (this.items.length == 0) {
+        return "Underflow";
+      }
+      return this.items.pop();
+    }
+  
+    top() {
+      return this.items[this.items.length - 1];
+    }
+  
+    empty() {
+      return this.items.length == 0;
+    }
   }
 
-  top() {
-    return this.items[this.items.length - 1];
-  }
-
-  empty() {
-    return this.items.length == 0;
-  }
-}
-
-export { Stack };
+export {Stack}
