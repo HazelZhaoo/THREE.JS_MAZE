870bfe0520fe2765a93dcbb11966388c9a04e57a
OatLatte11 <109826753+HazelZhaoo@users.noreply.github.com>
2023-05-07 21:50:39 -0700

    Update cell.js

--- cell.js
+++ cell.js
@@ -1,40 +1,80 @@
-class Cell extends THREE.Object3D 
-{
-    constructor(x, y, size, color) 
-    {
-      super(); //the super constructor that inherit every feature from the parent class//
-      this.x = x;
-      this.y = y;
-      this.size = size;
-      const material = new THREE.MeshBasicMaterial({color});
-  
-      const frontWall = new THREE.Mesh(new THREE.PlaneGeometry(size, size), material);
-      frontWall.position.z = size / 2;
-      this.add(frontWall);
-  
-      const backWall = new THREE.Mesh(new THREE.PlaneGeometry(size, size),material);
-      backWall.position.z = -size / 2;
-      backWall.rotateY(Math.PI);
-      this.add(backWall);
-  
-      const leftWall = new THREE.Mesh(new THREE.PlaneGeometry(size, size), material);
-      leftWall.position.x = -size / 2;
-      leftWall.rotateY(-Math.PI / 2);
-      this.add(leftWall);
-  
-      const rightWall = new THREE.Mesh(new THREE.PlaneGeometry(size, size),material);
-      rightWall.position.x = size / 2;
-      rightWall.rotateY(Math.PI / 2);
-      this.add(rightWall);
+class Cell extends THREE.Object3D {
+  constructor(x, y, z, size) {
+    super();
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.size = size;
+    
+    const geometry = new THREE.PlaneGeometry(size, size);
+    const material = new THREE.MeshBasicMaterial({ color: "#770737", side: THREE.DoubleSide, depthTest: true });
+    
+    this.frontPlane = new THREE.Mesh(geometry, material);
+    this.frontPlane.position.y = -size/5;
+    this.add(this.frontPlane);
+
+    this.backPlane = new THREE.Mesh(geometry, material);
+    this.backPlane.rotateY(-Math.PI / 2);
+    this.backPlane.position.x = -size/2;
+    this.backPlane.position.z = -size/2;
+    this.add(this.backPlane);
+
+    this.rightPlane = new THREE.Mesh(geometry, material);
+    this.rightPlane.rotateZ(-Math.PI / 2);
+    this.rightPlane.position.z = -size;
+    this.add(this.rightPlane);
+
+    this.leftPlane = new THREE.Mesh(geometry, material);
+    this.leftPlane.rotateY(Math.PI / 2);
+    this.leftPlane.position.x = size/2;
+    this.leftPlane.position.z = -size/2;
+    this.add(this.leftPlane);
+    
+    this.position.set(x, y, z);
+  }
+ 
+  removeWall(direction) {
+    let wall , opp_wall;
+     
+    if (direction === "rightPlane") {
+      wall = this.rightPlane;
+    } else if (direction === "leftPlane") {
+      wall = this.leftPlane;
+    } else if (direction === "frontPlane") {
+      wall = this.frontPlane;
+    } else if (direction === "backPlane") {
+      wall = this.backPlane;
+    } else {
+      return null;
     }
+
+    this.remove(wall);
+     
+  }
   
-    removeWall(direction) {
-      const wall = this.children.find(child => child.name === direction);
-      if (wall) {
-        this.remove(wall);
-        wall.geometry.dispose();
-        wall.material.dispose();
-      }
+  getNeighborPosition(direction) {
+    const [x, y, z] = [this.x, this.y, this.z];
+    
+    if (direction === "frontPlane") {
+      return [this.frontPlane.position.x, this.frontPlane.position.y, this.frontPlane.position.z];
+    } else if (direction === "backPlane") {
+      return [this.backPlane.position.x, this.backPlane.position.y, this.backPlane.position.z];
+    } else if (direction === "leftPlane") {
+      return [this.leftPlane.position.x, this.leftPlane.position.y, this.leftPlane.position.z];
+    } else if (direction === "rightPlane") {
+      return [this.rightPlane.position.x, this.rightPlane.position.y, this.rightPlane.position.z];
     }
   }
-   export {Cell}
+
+  getOppositeDirection(direction) {
+    if (direction === "leftPlane") {
+      return "rightPlane";
+    } else if (direction === "rightPlane") {
+      return "leftPlane";
+    } else if (direction === "frontPlane") {
+      return "backPlane";
+    } else if (direction === "backPlane") {
+      return "frontPlane";
+    }
+  }
+}
